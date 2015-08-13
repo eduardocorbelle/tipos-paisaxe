78d20a8e38a57588c3b7d30f008dc9449f3fc5af
root <root@epimetheo-vb.inv.lugo.usc.es>
2015-08-13 11:19:24 +0200

    Cambios no Script4

--- Scripts/Script4.sh
+++ Scripts/Script4.sh
@@ -11,16 +11,30 @@ r.mask raster=concellos
 r.cross input=ClasesXeo,ClaseCuberta2,bioclima output=TiposPaisaxe
 r.mask -r
 
-########## Pasar a vector e simplificar (eliminar unidades menores de 1ha)
-r.to.vect -v input=TiposPaisaxe output=TiposPaisaxe type=area --o
-v.clean input=TiposPaisaxe output=TiposPaisaxeB tool=rmarea threshold=10000 --o
-v.generalize input=TiposPaisaxeB output=TiposPaisaxeC method=reumann threshold=26 --o
+########## Pasar a vector 
+r.to.vect -v input=TiposPaisaxe output=TiposPaisaxe type=area
 
 ########## Recortar o ámbito do POL
-v.overlay ainput=TiposPaisaxeC binput=Ambito operator=not output=TiposPaisaxeD --o
+v.overlay ainput=TiposPaisaxe binput=Ambito operator=not output=TiposPaisaxeR
+
+
+########## Simplificar (eliminar unidades menores de 1ha)
+v.generalize input=TiposPaisaxeR output=TiposPaisaxeB method=reumann threshold=26 --o
+v.clean input=TiposPaisaxeB output=TiposPaisaxeC tool=rmarea threshold=10000 --o
+
+
 
 ########## Exportar
 now=$(date +"%Y_%m_%d")
-result=TiposPaisaxe_$now.shp
+result=TiposPaisaxe_$now
 
-v.out.ogr in=TiposPaisaxeD output=./Resultados output_layer=$result
+v.out.ogr in=TiposPaisaxeC output=./Resultados output_layer=$result
+
+########## Limpeza do espazo de traballo
+g.remove type=vect name=TiposPaisaxe  -f
+g.remove type=vect name=TiposPaisaxeR -f
+g.remove type=vect name=TiposPaisaxeB -f
+
+########## Edición manual do mapa resultante: 
+## Eliminación dos restos do recorte co POL (diferencia de 12-16m debida as transformacións de sistema de referencia)
+## Separación da lenda en tres campos distintos da táboa de atributos.
