82e10a408e7c8ce30ba990d6abf7be3acec38650
root <root@epimetheo-vb.inv.lugo.usc.es>
2015-11-16 09:20:14 +0100

    Novo siose2011 finalmente introducido, figuras actualizadas, tipos por GAP actualizados

--- Informes/Informe1/TiposAreas.R
+++ Informes/Informe1/TiposAreas.R
@@ -1,7 +1,7 @@
 ## Script de R para resumir os datos de tipos de paisaxe por grandes áreas
 
 ## Cargamos os datos orixinais
-datos <- read.table("Informes/Informe1/TiposAreas_2015_10_02.txt", sep="|", header=FALSE)
+datos <- read.table("Informes/Informe1/TiposAreas_2015_11_16.txt", sep="|", header=FALSE)
 colnames(datos) <- c("GAP_code","GAP","CXn","CXt","CCn","CCt","Cln","Clt","Aream2")
 datos <- data.frame(datos, 
                     tipo = factor(paste(datos$CXt, ";", datos$CCt, ";", datos$Clt)))
